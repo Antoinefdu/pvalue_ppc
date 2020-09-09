eedb04cdf34966319de7d2875718f9bca7f5b35d
Antoine <antoine.fdu@gmail.com>
2020-09-09 19:53:19 +0100

    just a test for future functionality

--- start.py
+++
@@ -1,6 +0,0 @@
-import os
-
-database_name = input('Select a name for your database \n(Full text, no space, no ".db" required. Just type a word)')+'.db'
-print(database_name)
-
-os.system("py P-Value2.py")
